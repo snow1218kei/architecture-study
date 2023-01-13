3efbfb5a746915ec77de650325f787f7a8494bd1
yuuki-tsujimura <snow1218kei@gmail.com>
2023-01-13 12:50:32 +0900

    途中経過報告

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,11 @@ FROM golang:1.19
 WORKDIR /usr/src/app
 
 COPY go.mod ./
-RUN go mod download && go mod verify
+RUN go mod download
 
 COPY . .
-RUN go build -v -o /usr/src/app ./...
+RUN go get github.com/julienschmidt/httprouter
+RUN go build ./src/cmd/main.go
 
+EXPOSE 8080
 CMD ["app"]
--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func CheckHelth(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "ヘルスチェックに成功しました。")
+}
+
+func main() {
+	router := httprouter.New()
+	router.Get("/health", CheckHelth)
+	log.Fatal(http.ListenAndServe(":8000", router))
+
+	// server := http.Server{
+	// 	Addr: "127.0.0.1:8080",
+	// 	Handler: router,
+	// }
+
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatalf("失敗しました: %v", err)
+	// }
+}
